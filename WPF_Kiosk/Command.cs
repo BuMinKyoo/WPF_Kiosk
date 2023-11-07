a809a70d158c512a2d1196456be18db8c604edaf
BuMinKyoo <qnalsrb1@naver.com>
2023-11-08 00:01:03 +0900

    Window Loaded이벤트 MVVM패턴 개발중

---
+++ WPF_Kiosk/Command.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WPF_Kiosk
+{
+    public class Command : ICommand
+    {
+        Action<object> _execute;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public Command(Action<object> execute)
+        {
+            _execute = execute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
---
+++ WPF_Kiosk/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPF_Kiosk
+{
+    public class MainWindowViewModel
+    {
+        private Command _windowLoadedCommand;
+        public ICommand WindowLoadedCommand
+        {
+            get { return _windowLoadedCommand = new Command(OnWindowLoadedCommand); }
+        }
+
+        private void OnWindowLoadedCommand(object obj)
+        {
+            double screenWidth = SystemParameters.PrimaryScreenWidth;
+            double screenHeight = SystemParameters.PrimaryScreenHeight;
+        }
+    }
+}
