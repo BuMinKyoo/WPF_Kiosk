782d88cdc19337690864cd301abd0f40c8485c70
BuMinKyoo <qnalsrb1@naver.com>
2023-11-08 23:37:07 +0900

    KioskLock 화면 생성 및 클릭 이벤트

--- WPF_Kiosk/MainWindowViewModel.cs
+++ WPF_Kiosk/MainWindowViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,18 +11,56 @@ using System.Windows.Input;
 
 namespace WPF_Kiosk
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : INotifyPropertyChanged
     {
-        private Command _windowLoadedCommand;
-        public ICommand WindowLoadedCommand
+        public MainWindowViewModel()
         {
-            get { return _windowLoadedCommand = new Command(OnWindowLoadedCommand); }
+            WindowWidth = SystemParameters.PrimaryScreenWidth;
+            WindowHight = SystemParameters.PrimaryScreenHeight;
         }
 
-        private void OnWindowLoadedCommand(object obj)
+        private double _windowWidth;
+        public double WindowWidth
         {
-            double screenWidth = SystemParameters.PrimaryScreenWidth;
-            double screenHeight = SystemParameters.PrimaryScreenHeight;
+            get { return _windowWidth; }
+            set
+            {
+                _windowWidth = value;
+                Notify("WindowWidth");
+            }
         }
+
+        private double _windowHight;
+        public double WindowHight
+        {
+            get { return _windowHight; }
+            set
+            {
+                _windowHight = value;
+                Notify("WindowHight");
+            }
+        }
+
+        private Command _KioskLockMouseUp;
+        public ICommand KioskLockMouseUp
+        {
+            get { return _KioskLockMouseUp = new Command(OnKioskLockMouseUp); }
+        }
+
+        private void OnKioskLockMouseUp(object obj)
+        {
+            MessageBox.Show("클릭 이벤트 발생");
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void Notify([CallerMemberName] string propertyName = null)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
     }
 }
